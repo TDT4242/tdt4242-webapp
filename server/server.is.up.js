16855e3868a1b9226fe3ef438f095d23d4ea0f99
staklau <stein_aagee@hotmail.com>
2018-03-02 12:45:25 +0100

    change

--- server/server.is.up.js
+++ server/server.is.up.js
@@ -2,13 +2,16 @@ var supertest = require("supertest");
 
 var server = supertest.agent("http://localhost:3000");
 
+setTimeout(() => {
 
-server
-.get("/serverIsUp")
-.expect("Content-type",/json/)
-.end(function(err,res){
-  if (!res) {
-    var err = new Error("SERVER NOT RUNNING");
-    throw err;
-  }
-});
+  server
+  .get("/serverIsUp")
+  .expect("Content-type",/json/)
+  .end(function(err,res){
+    if (!res) {
+      var err = new Error("SERVER NOT RUNNING");
+      throw err;
+    }
+  });
+    
+}, 2000)
