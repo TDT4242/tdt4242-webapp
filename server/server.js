d8ff5628d1213f8a1b2230dd738e3c1435692d8a
staklau <stein_aagee@hotmail.com>
2018-03-03 22:07:14 +0100

    testupdate

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ var helmet = require('helmet');
 var config = require('./config/config.js');
 var Models = require('./models/index.js');
 
+// hello
 
 var app = express();
 var server = require('http').Server(app);
