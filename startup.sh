0ab9b17026f58a5ca87c611c91bbcdff7676147c
Sander <hanakmumakil23@gmail.com>
2018-03-04 12:42:15 +0100

    Adds part to script to finish deployment

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ docker pull mugthakk/tdt4242
 pwd
 ls -la
 cd server && ls -la && cd ..
-docker run -t -p 8080:3000 mugthakk/tdt4242 
+docker run -t -p 8080:3000 mugthakk/tdt4242 > /dev/null 2> /dev/null < /dev/null &
