a778af4fbfa653dfdb9b629f88b14976bacd81df
Sander <hanakmumakil23@gmail.com>
2018-03-04 12:00:24 +0100

    try to just restart container to make it pull image again and thus update le version

--- startup.sh
+++ startup.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-docker rm -f $(docker ps -a -q)
-docker rmi $(docker images -q)
-docker pull mugthakk/tdt4242
-pwd
-ls -la
-cd server && ls -la && cd ..
-docker run -t -p 3000:8080 mugthakk/tdt4242 
+docker restart $(docker ps -a -q)
