d2342a7352a5652abe53ba7a72ec9072ecbb0b99
Sander <hanakmumakil23@gmail.com>
2018-03-04 11:28:31 +0100

    kill all current containers, remove images, pull new and start that one

--- startup.sh
+++ startup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+docker rm -f $(docker ps -a -q)
+docker rmi $(docker images -q)
 docker pull mugthakk/tdt4242
 pwd
 ls -la
 cd server && ls -la && cd ..
-docker stop $(docker ps -a -q)
 docker run -t mugthakk/tdt4242 
