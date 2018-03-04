8b4c07515276255c42c0011ce362f6efb7fc7e1c
Sander <hanakmumakil23@gmail.com>
2018-03-04 12:03:12 +0100

    changing order of ports

--- startup.sh
+++ startup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker restart $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
+docker rmi $(docker images -q)
+docker pull mugthakk/tdt4242
+pwd
+ls -la
+cd server && ls -la && cd ..
+docker run -t -p 8080:3000 mugthakk/tdt4242 
