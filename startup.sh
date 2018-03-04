06b13c0c5f5f9d49c5f962e19e7a7e7d788e04d8
Sander <hanakmumakil23@gmail.com>
2018-03-04 13:46:47 +0100

    stony got a bony

--- startup.sh
+++ startup.sh
@@ -2,8 +2,7 @@
 
 docker rm -f $(docker ps -a -q)
 docker rmi $(docker images -q)
-docker build -t mugthakk/tdt4242:latest - < Dockerfile
 pwd
 ls -la
-cd server && ls -la && cd ..
+docker build -t mugthakk/tdt4242 - < Dockerfile
 docker run -t -p 8080:3000 mugthakk/tdt4242 > /dev/null 2> /dev/null < /dev/null &
